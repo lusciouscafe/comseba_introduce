68ea87beb7e6ab72c94720d9c6e669e17583a487
lusciouscafe <rnqhrud11@naver.com>
2023-11-22 19:18:33 +0900

    UPLOAD_//

--- backend/frontend/src/js/map.js
+++ backend/frontend/src/js/map.js
@@ -16,7 +16,7 @@ const marker = new kakao.maps.Marker({
 marker.setMap(map);
 
 const iwContent =
-    '<div style="padding:5px;">컴세바 써니랩학원 <br>인천지점<br><a href="https://map.kakao.com/link/map/컴세바 써니랩학원 인천지점,37.39808393961005, 126.64802612114711" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/컴세바 써니랩학원 인천지점,37.39808393961005, 126.64802612114711" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    '<div style="padding:5px;">컴세바 써니랩학원 <br>인천지점<br><a href="https://map.kakao.com/link/map/컴세바 써니랩학원 인천지점,37.39808393961005, 126.64802612114711" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/컴세바 써니랩학원 인천지점,37.39808393961005, 126.64802612114711" style="color:blue" target="_blank">길찾기</a></div>',
   iwPosition = new kakao.maps.LatLng(33.450701, 126.570667);
 
 const infowindow = new kakao.maps.InfoWindow({
